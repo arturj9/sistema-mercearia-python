33be1a3a7b36a695fba254713cb664cff1f6dfea
Artur Jardel Soares Gomes <arturjardel9@gmail.com>
2022-03-09 12:23:27 -0300

    Update Models.py

--- Models.py
+++ Models.py
@@ -1,20 +1,16 @@
 from datetime import datetime
-
 class Categoria:
     def __init__(self, categoria):
         self.categoria = categoria
-
 class Produtos:
     def __init__(self, nome, preco, categoria):
         self.nome = nome
         self.preco = preco
         self.categoria = categoria
-
 class Estoque:
     def __init__(self, produto: Produtos, quantidade):
         self.produto = produto
         self.quantidade = quantidade
-
 class Venda:
     def __init__(self, itensVendido: Produtos, vendedor, comprador, quantidadeVendida, data = datetime.now().strftime('%d/%m/%Y')):
         self.itensVendido = itensVendido
@@ -22,14 +18,12 @@ class Venda:
         self.comprador = comprador
         self.quantidadeVendida = quantidadeVendida
         self.data = data
-
 class Fornecedor:
     def __init__(self, nome, cnpj, telefone, categoria):
         self.nome = nome
         self.cnpj = cnpj
         self.telefone = telefone
         self.categoria = categoria
-
 class Pessoa:
     def __init__(self, nome, telefone, cpf, email, endereco):
         self.nome = nome
@@ -37,7 +31,6 @@ class Pessoa:
         self.cpf = cpf
         self.email = email
         self.endereco = endereco
-
 class Funcionario(Pessoa):
     def __init__(self, clt, nome, telefone, cpf, email, endereco):
         self.clt = clt
